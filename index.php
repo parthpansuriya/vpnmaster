eb3939b0a6ca5dfa97d9a1e05fefcf1cc0166585
parthpansuriya <37246367+parthpansuriya@users.noreply.github.com>
2021-10-19 19:21:02 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,330 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Meta Tags For Seo + Page Optimization -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="author" content="Themes Industry">
+        <!-- description -->
+        <meta name="description" content="App landing Page is a highly creative, modern, visually stunning and Bootstrap responsive multipurpose agency and HTML5 template with 14 ready home page demos.">
+        <!-- keywords -->
+        <meta name="keywords" content="creative, modern, clean, bootstrap responsive, html5, css3, portfolio, blog, agency, templates, multipurpose, one page, corporate, start-up, studio, branding, designer, freelancer, carousel, parallax, photography, personal, masonry, grid, faq">
+        <!-- Page Title -->
+        <title>File Manager - Manage your Files easily.</title>
+        <!-- Favicon -->
+        <link rel="icon" href="./img/favicon.png">
+        <!--css links-->
+        <link rel="stylesheet" href="./assets/css/bundle.min.css">
+        <link rel="stylesheet" href="./assets/css/wow.css">
+        <link rel="stylesheet" href="./assets/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="./assets/css/line-awesome.min.css">
+        <link rel="stylesheet" href="./assets/css/style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    </head>
+
+    <body data-spy="scroll" data-target=".navbar" data-offset="90">
+
+    <!--loader sec start-->
+    <div class="loader">
+        <div class="box">
+            <div class="loader-07"></div>
+        </div>
+    </div>
+    <!--loader sec end-->
+
+    <!--header sec start-->
+    <header class="head-sec">
+        <div class="container">
+        <div class="top-navigation">
+            <nav class="navbar navbar-expand-lg nav-up">
+                <a class="navbar-brand" href="#"><img src="./img/logo.png" alt="logo"/></a>
+
+                <div class="collapse navbar-collapse d-none d-lg-block" id="navbarNav">
+                    <ul class="navbar-nav horizontal-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link scroll" href="#banner-sec">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link scroll" href="#services-sec">Rate & Review</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link scroll" href="#app-clips">Screen Shots</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link scroll" href="#app">Features</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link scroll" href="#get-app-sec">Download</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="navigation-toggle ml-auto d-block d-lg-none">
+                    <ul class="slider-social toggle-btn" id="toggle-btn">
+                        <li class="animated-wrap">
+                            <a class="animated-element" href="javascript:void(0);">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+        </div>
+        <div class="broad">
+            <div class="close-nav">
+                <i class="las la-times"></i>
+            </div>
+            <nav class="navbar navbar-light">
+
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link scroll" href="#banner-sec"><i class="las la-angle-double-right"></i> Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link scroll" href="#services-sec"><i class="las la-angle-double-right"></i> Rate & Review</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link scroll" href="#app-clips"><i class="las la-angle-double-right"></i> Screen Shots</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link scroll" href="#app"><i class="las la-angle-double-right"></i> Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link scroll" href="#get-app-sec"><i class="las la-angle-double-right"></i> Download</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        </div>
+    </header>
+    <!--header sec end-->
+
+    <!--Banner sec Start-->
+    <section class="banner-sec" id="banner-sec">
+        <div class="container">
+            <div class="row no-gutters">
+                <div class="col-12 col-lg-6 banner-details text-center text-lg-left">
+                    <div class="banner-inner-content">
+                        <h4 class="banner-heading"><span>File Manager</span> App</h4>
+                        <p class="banner-text"> File Manager is a powerful, simple interface file manager that supports multiple routine operations </p>                    <a href="https://play.google.com/store/apps/details?id=com.File.manager&hl=en_IN" target="_blank" class="btn banner-btn">Install</a>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 banner-img">
+                    <img src="./img/slider-img1.png">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Banner sec end-->
+
+    <!--Services content start-->
+    <div id="services-sec" class="services-sec">
+        <div class="container">
+            <div class="row our-services">
+                <div class="col-12 col-md-6 s-cards">
+                    <div class="service-card text-center wow fadeInLeft" data-wow-duration="1s">
+                        <a href="#">
+                            <div class="image-holder">
+                                <b style="font-size: 30px;">4M+</b>
+                            </div>
+                            <h3 class="service-card-heading">Total Downloads</h3>
+                        </a>
+                    </div>
+
+                </div>
+                <div class="col-12 col-md-6 s-cards">
+                    <div class="service-card text-center wow fadeInRight" data-wow-duration="1s">
+                        <a href="#">
+                            <div class="image-holder">
+                                <b style="font-size: 30px;">10K+</b>
+                            </div>
+                            <h3 class="service-card-heading">Positive Reviews</h3>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 s-cards">
+                    <div class="service-card text-center wow fadeInLeft" data-wow-duration="1s">
+                        <a href="#">
+                            <div class="image-holder">
+                                <b style="font-size: 30px;">50K+</b>
+                            </div>
+                            <h3 class="service-card-heading">Daily Visitors</h3>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 s-cards">
+                    <div class="service-card text-center wow fadeInRight" data-wow-duration="1s">
+                        <a href="#">
+                            <div class="image-holder">
+                                <b style="font-size: 30px;">4.0</b>
+                            </div>
+                            <h3 class="service-card-heading">Rating</h3>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Services content end-->
+
+    <!--Screen content end-->
+    <section class="app-clips" id="app-clips" style="background-color: #fdd8a4;border-bottom-right-radius: 27%;">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-10 col-lg-8 offset-md-1 offset-lg-2 app-clips-details" >
+                    <h3 class="heading text-center">ScreenShots</h3>
+                    <p class="detail-text text-center"></p>
+                </div>
+            </div>
+            <div class="app-clips-slider owl-carousel owl-theme">
+                <div class="item">
+                    <img src="./img/screen1.jpg">
+                </div>
+                <div class="item">
+                    <img src="./img/screen2.jpg">
+                </div>
+                <div class="item">
+                    <img src="./img/screen3.jpg">
+                </div>
+                <div class="item">
+                    <img src="./img/screen4.jpg">
+                </div>
+                <div class="item">
+                    <img src="./img/screen5.jpg">
+                </div>
+                <div class="item">
+                    <img src="./img/screen6.jpg">
+                </div>
+                <div class="item">
+                    <img src="./img/screen7.jpg">
+                </div>
+                <div class="item">
+                    <img src="./img/screen8.jpg">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Screen content end-->
+
+
+    <!--App Section-->
+    <section id="app" class="app-sec">
+        <div class="container">
+            <!--Heading-->
+            <div class="row">
+                <div class="col-md-12 text-center wow fadeIn app-details">
+                    <div class="col-12 col-md-10 col-lg-8 offset-md-1 offset-lg-2" >
+                        <h3 class="heading text-center">Features</h3>
+                        <p class="detail-text text-center">File manager app is free, really easy to use with file organizer by category: Images, Music, Movies, Documents, Apps With this app manager for Android, you can easily manage your files and folders on device, manage local and remote/cloud storage.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!--App deatil-->
+            <div class="row align-items-center text-center app-features-list">
+
+                <div class="col-lg-4 mb-5 mb-lg-0 wow fadeInLeft">
+                    <!--App deatil item-->
+                    <div class="app-feature">
+                        <i class="fa fa-cogs" aria-hidden="true"></i>
+                        <h4 class="mb-3">Manage File</h4>
+                        <p>Manage your files like you do on your desktop or laptop using Multiple Select, Cut/Copy/Paste, Move, Create, Delete, Rename, Search, Share, Send, Hide, Create Shortcut, and Bookmark.</p>
+                    </div>
+                    <div class="app-feature">
+                        <i class="fa fa-paint-brush" aria-hidden="true"></i>
+                        <h4 class="mb-3">Theme Options</h4>
+                        <p>Customized your phone with awesome themes. Every day new themes with new lock screens, new icons, moded gadgets & more...</p>
+                    </div>
+                    <!--App deatil item-->
+                </div>
+                <!--app slider-->
+                <div class="col-lg-4 mb-5 mb-lg-0 wow fadeInUp">
+                    <!--app Image-->
+                    <div class="app-image">
+                        <img src="./img/iphone-img.png" alt="image">
+                        <div id="app-slider" class="owl-carousel owl-theme">
+                            <div class="item">
+                                <img src="./img/img1.jpg" alt="">
+                            </div>
+                            <div class="item">
+                                <img src="./img/img2.jpg" alt="">
+                            </div>
+                            <div class="item">
+                                <img src="./img/img3.jpg" alt="">
+                            </div>
+                            <div class="item">
+                                <img src="./img/img4.jpg" alt="">
+                            </div>
+                            <div class="item">
+                                <img src="./img/img5.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 wow fadeInRight">
+                    <!--App deatil item-->
+                    <div class="app-feature">
+                        <i class="fa fa-lock" aria-hidden="true"></i>
+                        <h4 class="mb-3">File Encryption</h4>
+                        <p> AppLock has PIN and Pattern lock, choose your favorite style to lock apps. Pattern lock is more easier and faster to unlock. PIN lock has random keyboard. Supports convenient and powerful lock with fingerprint sensor.</p>
+                    </div>
+                    <!--App deatil item-->
+                    <div class="app-feature">
+                        <i class="fa fa-exchange" aria-hidden="true"></i>
+                        <h4 class="mb-3">File Transfer</h4>
+                        <p>Data manager & data transfer: file sharing using various tools: “send anywhere”, email, sms, … & file transfer to PC via http file transfer protocol.</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+    <!--App Section End-->
+
+
+    <!--Get App Banner Start-->
+    <section id="get-app-sec" class="get-app-sec" style="background: url('./img/banner2.jpg');">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-10 col-lg-8 offset-md-1 offset-lg-2 get-app-details text-center" >
+                    <!-- <p class="sub-heading text-center">- Most flexible one page -</p> -->
+                    <h3 class="heading text-center">Get Mobile App</h3>
+                    <p class="detail-text text-center">File manager is the best file explorer, file tool for android device manager with powerful features: Copy, cut, paste, rename, compress, transfer, download … </p>
+                    <a href="https://play.google.com/store/apps/details?id=com.File.manager&hl=en_IN" target="_blank" class="get-app-btn"><i class="fa fa-android"></i>Google Play</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!---Get App Banner Start End-->
+
+    <!--footer sec start-->
+    <footer class="footer-sec" id="footer-sec">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center">
+                    <div class="footer-icons d-flex">
+                        <a target="_blank" href="https://play.google.com/store/apps/developer?id=FILE+MANAGER&hl=en_IN" class="wow fadeInUp"><i class="fa fa-copyright"></i> All Rights is reserved by <b style="margin-left: 5px;margin-top: 0px;"> FILE MANAGER</b></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--footer sec end-->
+
+
+    <script src="./assets/js/bundle.min.js"></script>
+    <script src="./assets/js/owl.carousel.min.js"></script>
+    <script src="./assets/js/wow.min.js"></script>
+    <script src="./assets/js/parallaxie.min.js"></script>
+    <!--counter js links-->
+    <script src="./assets/js/script.js"></script>
+    </body>
+
+</html>
